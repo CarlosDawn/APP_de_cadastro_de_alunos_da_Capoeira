8ec348b9929d3a9b495438d73edf853b7d3d6576
CarlosDawn <155602345+CarlosDawn@users.noreply.github.com>
2024-02-01 17:43:16 -0300

    Update cadastraAluno.py

--- cadastraAluno.py
+++ cadastraAluno.py
@@ -2,6 +2,7 @@ from psycopg2 import OperationalError
 from tkinter import messagebox
 
 def cadastrar(conexao, Nome_completo_btn, Data_nasc_btn, Nome_pai_btn, Nome_mae_btn, Endereco_btn, Bairro_btn, Data_inicio_btn):
+    path = " "
     try:
         cursor = conexao.cursor()
         query = f"""INSERT INTO alunos
@@ -11,7 +12,8 @@ VALUES (DEFAULT, '{Nome_completo_btn}',
 '{Nome_mae_btn}', 
 '{Endereco_btn}', 
 '{Bairro_btn}', 
-'{Data_inicio_btn[6:10]}{Data_inicio_btn[3:5]}{Data_inicio_btn[0:2]}')"""
+'{Data_inicio_btn[6:10]}{Data_inicio_btn[3:5]}{Data_inicio_btn[0:2]}',
+'{path}')"""
         cursor.execute(query)
         conexao.commit()
         cursor.close()
